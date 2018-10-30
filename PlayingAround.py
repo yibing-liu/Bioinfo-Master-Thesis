003d76c18186056545b1788dcf052bf5bc594903
Yibing Liu <Yibing@Yibings-MacBook-Pro-2.local>
2018-10-31 00:38:02 +0100

    tiny fix

--- PlayingAround.py
+++ PlayingAround.py
@@ -1,6 +1,34 @@
 
 import os
 
+#generate file fileName
+def getRawCountsFileName(a, i):
+    rawCountsFileName = "TVEMB" + a + "_" + i +".250K.101.sorted.count-t"
+    return rawCountsFileName
+
+#get raw_counts files of given embryo
+def getRawCountsFile(rawCountsFileName):
+    for file in os.listdir(path):
+        fileName = os.path.basename(file)
+        if fileName == rawCountsFileName:
+            return file
+
+#select interested columns in file
+def getColumn(rawCountsFile):
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 #files in raw_counts folder
 rawCountsFile = []
 path = os.getcwd()+"/Data/raw_counts"
